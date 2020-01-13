ddb329ff6bbb9618728a091860c5d6a043f4eac4
Andrew Wong <andrew.j.wong@outlook.com>
2020-01-13 14:11:50 +1100

    Refactor, add interrupts, add debouncing

--- BlueClick_BLE/BlueClick_BLE.ino
+++ BlueClick_BLE/BlueClick_BLE.ino
@@ -11,6 +11,14 @@
    Forked: https://github.com/neilbags/arduino-esp32-BLE-MIDI
 */
 
+#define NOTE_A 0x3c
+#define PIN_A 32
+
+#define NOTE_B 0x3d
+#define PIN_B 33
+
+/////////////////////////////////////////////////////////////////
+
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
@@ -19,33 +27,26 @@
 #define SERVICE_UUID        "03b80e5a-ede8-4b33-a751-6ce34ec4c700"
 #define CHARACTERISTIC_UUID "7772e5db-3868-4112-a1a9-f2669d106bf3"
 
-#define NOTE_A 0x3c
-#define NOTE_B 0x3d
+void IRAM_ATTR sendNote(uint8_t note);
+void IRAM_ATTR sendNote(uint8_t note, uint8_t velocity);
 
 BLECharacteristic *pCharacteristic;
-bool deviceConnected = false;
 
-uint8_t midiPacket[] = {
-  0x80,  // header
-  0x80,  // timestamp, not implemented
-  0x00,  // status
-  0x3c,  // 0x3c == 60 == middle c
-  0x00   // velocity
-};
+volatile bool deviceConnected = false;
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
+      // Serial.println("Connected to a client");
       deviceConnected = true;
     };
 
     void onDisconnect(BLEServer* pServer) {
+      // Serial.println("Disconnected from a client");
       deviceConnected = false;
     }
 };
 
 void setupBLE() {
-  Serial.begin(115200);
-
   BLEDevice::init("BlueClick BLE");
 
   // Create the BLE Server
@@ -75,51 +76,72 @@ void setupBLE() {
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
   pAdvertising->addServiceUUID(pService->getUUID());
   pAdvertising->start();
+  // Serial.println("Ready to receive BLE connections");
 }
 
-void setup() {
-  Serial.begin(115200);
-  setupBLE();
+
+void IRAM_ATTR SEND_A() {
+  sendNote(NOTE_A);
+}
+
+void IRAM_ATTR SEND_B() {
+  sendNote(NOTE_B);
 }
 
 /*
    Send note at default velocity 127
 */
-void sendNote(uint8_t note) {
+void IRAM_ATTR sendNote(uint8_t note) {
   sendNote(note, 127);
 }
 
 /*
     Send note at a given velocity
 */
-void sendNote(uint8_t note, uint8_t velocity) {
-  midiPacket[3] = note; // note
-  midiPacket[4] = velocity;  // velocity
+void IRAM_ATTR sendNote(uint8_t note, uint8_t velocity) {
+  // Debounce
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time < 150) return;
+  last_interrupt_time = interrupt_time;
+
+  if (!deviceConnected) return;
+  // Serial.println("Sending note");
+
+  uint8_t midiPacket[] = {
+    0x80,    // header
+    0x80,    // timestamp (not implemented)
+    0x00,    // status
+    note,    // note
+    velocity // velocity
+  };
 
   // note down
-  midiPacket[2] = 0x90; // note down, channel 0
+  midiPacket[2] = 0x90;                     // note down, channel 0
   pCharacteristic->setValue(midiPacket, 5); // packet, length (bytes)
-  pCharacteristic->notify(); // emit
+  pCharacteristic->notify();                // emit
   delay(100);
 
   // note up
-  midiPacket[2] = 0x80; // note up, channel 0
+  midiPacket[2] = 0x80;                     // note up, channel 0
   pCharacteristic->setValue(midiPacket, 5); // packet, length (bytes)
-  pCharacteristic->notify(); // emit
-  delay(100);
-}
-
-bool state = true;
-void loop() {
-  // Pin interrupts
-  if (deviceConnected) {
-    if (state) {
-      sendNote(NOTE_A);
-      state = false;
-    } else {
-      sendNote(NOTE_B);
-      state = true;
-    }
-  }
+  pCharacteristic->notify();                // emit
 
 }
+
+
+void setup() {
+  Serial.begin(115200);
+
+  // Slow down the clock speed, to save power
+  setCpuFrequencyMhz(80);
+
+  setupBLE();
+  
+  pinMode (PIN_A, INPUT_PULLUP);
+  pinMode (PIN_B, INPUT_PULLUP);
+  attachInterrupt(PIN_A, SEND_A, FALLING);
+  attachInterrupt(PIN_B, SEND_B, FALLING);
+}
+
+void loop() {}
